e350a95f783c55bd98c883178b98a40c57b8e0a4
hdrpknc <hdrpknc@users.noreply.github.com>
2019-01-13 22:05:43 +0100

    adding arguments

--- veact.js
+++ veact.js
@@ -1,14 +1,24 @@
-window.alert('Hallo Welt!');
-function makeElement(type, text) {
-    const el = document.createElement(type);
-    const textNode = document.createTextNode(text);
-    
-    el.appendChild(textNode);
-    
-    return el;
-  }
+function makeElement(type, props, text) {
+  const el = document.createElement(type);
   
-  const h1 = (text) => makeElement(`h1`, text);
+  Object.keys(props).forEach(prop => {
+   el[prop] = props[prop];
+  });
   
-  // and then
-  document.body.appendChild(h1(`Hello, world.`));
+  const textNode = document.createTextNode(text);
+  
+  el.appendChild(textNode);
+  
+  return el;
+ }
+ 
+ const h1 = (...args) => makeElement(`h1`, ...args);
+ 
+ 
+ // and then ...
+ document.body.appendChild(
+  h1(
+    { className: `title` },
+    `Hello, world.`,
+  )
+ );
