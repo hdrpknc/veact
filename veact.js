3d2ce3be83997dc371eecdc8a6d11cb018c0ad0d
Pekince <Hidir.Pekince@all-for-one.com>
2019-01-17 18:35:54 +0100

    simple framework done

--- veact.js
+++ veact.js
@@ -1,24 +1,82 @@
-function makeElement(type, props, text) {
-  const el = document.createElement(type);
-  
-  Object.keys(props).forEach(prop => {
-   el[prop] = props[prop];
-  });
-  
+const attributeExceptions = [
+  "role",
+];
+
+function appendText(el, text) {
   const textNode = document.createTextNode(text);
-  
   el.appendChild(textNode);
-  
+}
+
+function appendArray(el, children) {
+  children.forEach((child) => {
+    if (Array.isArray(child)) {
+      appendArray(el, child);
+    } else if (child instanceof window.Element) {
+      el.appendChild(child);
+    } else if (typeof child === `string`) {
+      appendText(el, child);
+    }
+  });
+}
+
+function setStyles(el, styles) {
+  if (!styles) {
+    el.removeAttribute(`styles`);
+    return;
+  }
+
+  Object.keys(styles).forEach((styleName) => {
+    if (styleName in el.style) {
+      el.style[styleName] = styles[styleName]; // eslint-disable-line no-param-reassign
+    } else {
+      console.warn(`${styleName} is not a valid style for a <${el.tagName.toLowerCase()}>`);
+    }
+  });
+}
+
+//the first argument will always be a tag name, such as "h1"
+//the second argument could be: 
+//  *An object defining the props for the element 
+//  *A string defining some text to display
+//  *A single element
+//  *An array of elements
+//After the second argument will be an element or an array of elements
+function makeElement(type, textOrPropsOrChild, ...otherChildren) {
+  const el = document.createElement(type);
+
+  if (Array.isArray(textOrPropsOrChild)) {
+    appendArray(el, textOrPropsOrChild);
+  } else if (textOrPropsOrChild instanceof window.Element) {
+    el.appendChild(textOrPropsOrChild);
+  } else if (typeof textOrPropsOrChild === `string`) {
+    appendText(el, textOrPropsOrChild);
+  } else if (typeof textOrPropsOrChild === `object`) {
+    Object.keys(textOrPropsOrChild).forEach((propName) => {
+      if (propName in el || attributeExceptions.includes(propName)) {
+        const value = textOrPropsOrChild[propName];
+
+        if (propName === `style`) {
+          setStyles(el, value);
+        } else if (value) {
+          el[propName] = value;
+        }
+      } else {
+        console.warn(`${propName} is not a valid property of a <${type}>`);
+      }
+    });
+  }
+
+  if (otherChildren) appendArray(el, otherChildren);
+
   return el;
- }
- 
- const h1 = (...args) => makeElement(`h1`, ...args);
- 
- 
- // and then ...
- document.body.appendChild(
-  h1(
-    { className: `title` },
-    `veact.js`,
-  )
- );
+}
+
+
+//html elements which veact.js supports
+ const h1 = (...args) => makeElement("h1", ...args);
+ const button = (...args) => makeElement("button", ...args);
+ const div = (...args) => makeElement("div", ...args);
+ const a = (...args) => makeElement("a", ...args);
+ const header = (...args) => makeElement("header", ...args);
+ const p = (...args) => makeElement("p", ...args);
+ const span = (...args) => makeElement("span", ...args);
