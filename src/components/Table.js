5d6debeaabce7fff7f19439988ea296bc83c93b5
hdrpknc <hdrpknc@users.noreply.github.com>
2019-01-20 00:02:30 +0100

    fix TableRow.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
 import { div } from '../libs/veact.js';
 import { TableRow } from './TableRow.js';
 
-export const Table = props => div({ className: `skill-table` }, TableRow(props.rows), TableRow(props.rows));
+export const Table = props => div({ className: `skill-table` }, TableRow(props.rows[0]), TableRow(props.rows[1]));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { App } from './components/App.js';
 
 const someData = {
-    rows: 2,
+    rows: [{name : "eins", id: "1"}, {name : "zwei", id: "2"}],
     version: 1
   };
   
