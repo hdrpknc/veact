646616d0203bec7c7fe4a8b46e3931379f27385a
hdrpknc <hdrpknc@users.noreply.github.com>
2019-01-19 22:51:52 +0100

    fix TableRow.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
 import { div } from '../libs/veact.js';
-import { tablerow } from './TableRow.js';
+import { TableRow as tablerow } from './TableRow.js';
 
 export const Table = props => div({ className: `skill-table` }, tablerow(props.rows), props.rows);
